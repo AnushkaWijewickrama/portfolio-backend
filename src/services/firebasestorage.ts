3fc49d63f854697d96735b4c2122680d3957b200
Anushka Wijewickrama <kushananushka12345@gmail.com>
2025-03-14 07:49:34 +0530

    Update firebasestorage.ts

--- src/services/firebasestorage.ts
+++ src/services/firebasestorage.ts
@@ -1,10 +1,24 @@
 import { Storage, Bucket, File } from '@google-cloud/storage';
-import * as path from 'path';
 import { v4 as uuidv4 } from 'uuid';
 import * as admin from 'firebase-admin';
 
+
+const credentials = {
+    "type": "service_account",
+    "project_id": process.env.FIREBASE_PROJECT_ID,
+    "private_key_id": process.env.FIREBASE_PRIVATE_KEY_ID,
+    "private_key": process.env.FIREBASE_PRIVATE_KEY,
+    "client_email": process.env.FIREBASE_CLIENT_EMAIL,
+    "client_id": process.env.FIREBASE_CLIENT_ID,
+    "auth_uri": process.env.FIREBASE_AUTH_URI,
+    "token_uri": process.env.FIREBASE_TOKEN_URI,
+    "auth_provider_x509_cert_url": process.env.FIREBASE_AUTH_PROVIDER_CERT,
+    "client_x509_cert_url": process.env.FIREBASE_CLIENT_CERT,
+    "universe_domain": "googleapis.com"
+}
+
 const storage = new Storage({
-    keyFilename: path.join(__dirname, '../../json/fir-78726-firebase-adminsdk-34sx9-4fc57313af.json'),
+    credentials: credentials,
     projectId: 'fir-78726',
 });
 
