d203945fa84b3557195488a9cddfc108b2f17c2a
Anushka Wijewickrama <kushananushka12345@gmail.com>
2026-04-17 11:25:24 +0530

    check data

--- src/routes/user.ts
+++ src/routes/user.ts
@@ -3,7 +3,7 @@ const userController = require('../controllers/user');
 const router = express.Router();
 const verifyToken = require('../middleware/authMiddleware');
 
-router.get('/user/', userController.getUser);
+router.get('/usera/', userController.getUser);
 router.delete('/user/:id', verifyToken, userController.deleteUserHandler);
 router.post('/user/login', userController.login);
 router.post('/user/register', verifyToken, userController.register);
